5825e69bb0ca56102b27a7bca7dc1efe57fd5178
Amy Kapernick <amy@kapers.dev>
2021-03-29 13:00:16 +0800

    fixed issue with parsing sectionId

--- utils/parseSection.js
+++ utils/parseSection.js
@@ -1,20 +1,22 @@
 import { parse } from 'date-fns';
 
-const parseWeek = (section) => {
-	parse(section, `dd-MMM-yyyy`, new Date());
-};
+const parseWeek = (section) => (
+	parse(section, `dd-MMM-yyyy`, new Date())
+);
 
-const parseMonth = (section) => {
-	parse(section, `MMM-yyyy`, new Date());
-};
+const parseMonth = (section) => (
+	parse(section, `MMM-yyyy`, new Date())
+);
 
-const parseSection = (section = ``) => {
+const parseSection = (section) => {
 	if (section.match(/\d{2}-\w{3}-\d{4}/)) {
 		return parseWeek(section);
 	}
 	if (section.match(/\w{3}-\d{4}/)) {
 		return parseMonth(section);
 	}
+
+	console.log(`Error, section is invalid`);
 
 	return `Error, section is invalid`;
 };
